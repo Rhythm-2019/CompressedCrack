088b373a9016a41a6eefb0d7ba14ad7091b8f074
Rhythm-2019 <rhythm_2019@163.com>
2022-11-19 09:37:10 +0800

    [style] Modify the code according to Google Python Style

--- README.md
+++ README.md
@@ -2,17 +2,26 @@
 
 Compressed Crack is a simple tool to help you crack password zip and rar files.
 
-Author: Thanh Minh
+Clone from [this project](https://github.com/mnismt/CompressedCrack), and make functional enhancements on the basis of the original 
+
+## Features:
+* Muti-Processe to brute compress file.
+* Completely open source
+* Code according to the [Google Python Style Guide](https://google.github.io/styleguide/pyguide.html)
+
+
 
 ## Requirements:
 
 [Python 3.x](https://www.python.org/downloads/)
 
+[Unrar](https://www.rarlab.com/rar_add.htm): This is a RAR decompression program written in C++. If your operating system is Windows, no action is required. If you are using a Linux operating system, you need to install Unrar manually. For the installation method, refer to the official RARLIB documentation
+
 ## Install
 
 ```
 apt-get -y install git
-git clone https://github.com/thanhminh6996/CompressedCrack.git
+git clone https://github.com/Rhythm-2019/CompressedCrack
 cd ./CompressedCrack
 ```
 ## Use
@@ -28,3 +37,7 @@ optional arguments:
                         Insert the file path of compressed file
                         
 ```                       
+
+## Ref
+* [Google Python Style Guide](https://google.github.io/styleguide/pyguide.html)
+* [Python Multiprocessing: The Complete Guide](]https://superfastpython.com/multiprocessing-in-python/)
--- crack.py
+++ crack.py
@@ -1,3 +1,45 @@
+# MIT License
+
+# Copyright (c) 2022 Rhythm-2019
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Crack Rar or Zip file
+
+This is a simple script to crack rar or zip file with password
+
+Basic Logic:
+* Generate a series of passwords based on user input or default parameters and try them repeatedly
+* Improve efficiency with multi-processing
+
+Usage:
+
+Run follow command in you shell:
+
+$ crack.py <filepath>
+
+or 
+
+$ crack.py <filepath> <start len> <max len> [character]
+
+"""
+
 import zipfile
 import rarfile
 import os
@@ -6,134 +48,195 @@ from threading import Thread
 import argparse
 from itertools import product
 import time
+import urllib.request
+import shutil
 
 parser = argparse.ArgumentParser(description='CompressedCrack', epilog='Use the -h for help')
 parser.add_argument('-i','--input', help='Insert the file path of compressed file', required=True)
 parser.add_argument('rules', nargs='*', help='<min> <max> <character>')
 
 # Const Character
-CHARACTER = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=~`[]{}|\\:;\"'<>,.?/"
+_DEFAULT_CHARACTER = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=~`[]{}|\\:;\"'<>,.?/"
 
-class Check:
-    def __init__(self, Arg):
+# UnRAR.exe download path
+_UNRAR_WIN32_DOWNLOAD_URL = "https://www.rarlab.com/rar/unrarw32.exe"
+
+# UnRAR file name
+_UNRAR_FILE_NAME_IN_WIN32 = "UnRAR.exe"
+_UNRAR_FILE_NAME_IN_LINUX = "unrar"
+
+class CmdAgrs(object):
+    """Command args holder
+    
+    Attributes:
+        type: file type(rar or zip)
+        if_use_custom_rules: if user use custom rule
+        start_length: password start length
+        max_length: password max length
+        character: passwprd character
+        location: file path
+    """
+        
+    def __init__(self, location: str, rule: list(str)) -> None:
         self.type = None
-        self.rules = False
-        self.startLength = None
-        self.maxLength = None
+        self.if_use_custom_rules = False
+        self.start_length = None
+        self.max_length = None
         self.character = None
-        # Check Rules
-        if len(Arg) >= 4:
-            self.getData(Arg)
-            self.rules = True
-        elif len(Arg) == 0 or len(Arg) > 2:
+        self.location = None
+
+        def error_handle(msg: str = None):
+            if msg is not None:
+                print(msg)
             parser.print_help()
             parser.exit()
+
+        if not location:
+            error_handle()
+        if rule and (len(rule) != 2 or len(rule != 3)):
+            error_handle('rule is <start length> <max length> [character]')
+
+        # Check Rules
+        self.location = location
+        if rule:
+            # Normal input: crack.py filepath start_len max_len [character]
+            try:
+                self.start_length = int(rule[0])
+                self.max_length = int(rule[1])
+            except ValueError:
+                error_handle('start length or max length Value Error')
+            if self.start_length > self.max_length:
+                error_handle('start length should lower than max lengthLength Error')
+
+            self.character = rule[2]
+            self.if_use_custom_rules = True
+
         # Check File Exist
-        if (self.CheckFileExist(Arg)):
-            self.getType(Arg)
-        else:
-            print ('No such file or directory: ',Arg[1])
+        if not os.path.isfile(self.location):
+            print('No such file or directory: ',args[1])
             parser.exit()
 
-    def CheckFileExist(self, Arg):
-        if (os.path.isfile(Arg[1])):
-            return True
+        if os.path.splitext(self.location)[1] == ".rar" or os.path.splitext(self.location)[1]==".zip":
+            self.type = os.path.splitext(self.location)[1]
         else:
-            return False
+            error_handle('file extension should be rar or zip')
 
-    def getData(self, Arg):
-        try:
-            self.startLength = int(Arg[2])
-            self.maxLength = int(Arg[3])
-        except ValueError:
-            print ('Value Error')
-            parser.exit()
-        if self.startLength > self.maxLength:
-            print ('Length Error')
-            parser.exit()
-        if len(Arg) == 5:
-            self.character = Arg[4]
+        # Check charactor
+        if self.character is None:
+            self.character = _DEFAULT_CHARACTER
     
-    def getType(self, Arg):
-        if os.path.splitext(Arg[1])[1] == ".rar" or os.path.splitext(Arg[1])[1]==".zip":
-            self.type = os.path.splitext(Arg[1])[1]
-        else:
-            print ('Extension Error')
-            parser.exit()
+class Handler(object):
+    """ Commpressed file brute handler
 
-class Handler:
-    def __init__(self, rules, typeCompress, startLength, maxLength, character):
-        self.rules = rules
-        self.location = sys.argv[2]
-        self.type = typeCompress
-        self.startLength = startLength
-        self.maxLength = maxLength
-        if not character:
-            self.character = CHARACTER
+    Attrubutes:
+        _start_time: start time
+        _cmd_args: a instance of CmdArgs
+        _file_crack: decompression algorithm
+    """
+
+    """ find proganm location, just find in current dir or PATH
+    Args:
+        progarm: progarm name
+    Return:
+        str: progarm abs path
+    """
+    @staticmethod
+    def which(program: str) -> str:
+        def is_exe(fpath):
+            return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+        fpath, fname = os.path.split(program)
+        if fpath:
+            if is_exe(program):
+                return program
         else:
-            self.character = character
+            for path in os.environ["PATH"].split(os.pathsep):
+                exe_file = os.path.join(path, program)
+                if is_exe(exe_file):
+                    return exe_file
+        return None
+
+
+    def __init__(self, cmd_args: CmdAgrs) -> None:
+        self._start_time = None
+        self._cmd_args = cmd_args
         self.result = False
 
-        self.GetFile()
-        self.CheckRules()
+        self._file_crack = zipfile.ZipFile(self.location) if self._cmd_args.type == '.zip' else rarfile.RarFile(self.location)
 
-    def GetFile(self):
-        # Khai báo file
-        if self.type == '.zip':
-            self.FileCrack = zipfile.ZipFile(self.location)
+        if self._cmd_args.type == '.rar':
+            self._download_unrar()
+        
+
+    def _download_unrar(self) -> None:
+        if sys.platform == "linux" or sys.platform == "linux2":
+            # linux
+            if self.__class__.which(_UNRAR_FILE_NAME_IN_LINUX) is None:
+                print("please install unrar")
+                parser.exit()
+        elif sys.platform == "win32":
+            # Windows...
+            if os.path.exists(_UNRAR_FILE_NAME_IN_WIN32) is None:
+                with urllib.request.urlopen(_UNRAR_WIN32_DOWNLOAD_URL) as response, open(_UNRAR_FILE_NAME_IN_WIN32, 'wb') as out_file:
+                    shutil.copyfileobj(response, out_file)
+            rarfile.UNRAR_TOOL = _UNRAR_FILE_NAME_IN_WIN32
         else:
-            self.FileCrack = rarfile.RarFile(self.location)
+            print("just support windows and centos, y")
+            parser.exit()
 
-    def Brute(self,password):
+    def _brute(self, password: str) -> None:
         try:
-            if self.type == '.zip':
+            if self._cmd_args.type == '.zip':
                 tryPass = password.encode()
             else:
                 tryPass = password
-            print (tryPass)
-            self.FileCrack.extractall(pwd=tryPass)
-            print ('Complete')
-            print('Time:',time.process_time() - self.start_time,'s')
-            print ('Password:',password)
+            print(tryPass)
+            self._file_crack.extractall(pwd=tryPass)
+            # Success
+            print('Complete')
+            print('Time:',time.process_time() - self._start_time,'s')
+            print('Password:',password)
             self.result = True
         except:
+            # Failed
             pass
 
-    def CheckRules(self):
-        self.start_time = time.process_time()
-        print ('Cracking...')
-        if not self.rules:
+    def execute(self) -> None:
+        self._start_time = time.process_time()
+        print('Cracking...')
+        if not self._cmd_args.if_use_custom_rules:
             length = 1
             while True:
-                self.SendRequest(length)
+                self._send_request(length)
                 if self.result:
                     return
                 length += 1
         else:
-            for length in range(self.startLength, self.maxLength + 1):
-                self.SendRequest(length)
+            for length in range(self._cmd_args.start_length, self._cmd_args.max_length + 1):
+                self._send_request(length)
                 if self.result:
                     return
             if not self.result:
-                print ('Cannot find password with this rules')
+                print('Cannot find password with this rules')
                 return
 
-    def SendRequest(self,length):
-        listPass = product(self.character, repeat=length)
+    def _send_request(self, length: int) -> None:
+        # Use porduct to avoid out of memory
+        listPass = product(self._cmd_args.character, repeat=length)
         for Pass in listPass:
             tryPass = ''.join(Pass)
             # Multi Thread:
             # nThread = Thread(target=self.Brute, args=(tryPass, ))
             # nThread.start()
-
+            # TODO GPU 加速、多进程
             # Single Thread: 
-            self.Brute(tryPass)
+            self._brute(tryPass)
             if self.result:
                 return
 def main():
-    check = Check(sys.argv[1:])
     args = parser.parse_args()
-    rarfile.UNRAR_TOOL = "UnRAR.exe"
-    Handling = Handler(check.rules, check.type, check.startLength, check.maxLength, check.character)
+    handler = Handler(CmdAgrs(args[0], args[1]))
+    handler.execute()
+    
 if __name__ == '__main__':
     main()
